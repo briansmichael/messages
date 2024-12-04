97f511b1b80e2d369ebdbc408af87c15b15dbc8f
Brian Michael <brian@cajunbug.net>
2024-12-04 11:01:22 -0500

    Updating to springboot 3.4.0

--- src/test/java/com/starfireaviation/messages/RestTemplateResponseErrorHandler.java
+++ src/test/java/com/starfireaviation/messages/RestTemplateResponseErrorHandler.java
@@ -21,17 +21,14 @@ import org.springframework.web.client.ResponseErrorHandler;
 
 import java.io.IOException;
 
-import static org.springframework.http.HttpStatus.Series.CLIENT_ERROR;
-import static org.springframework.http.HttpStatus.Series.SERVER_ERROR;
-
 public class RestTemplateResponseErrorHandler
         implements ResponseErrorHandler {
 
     @Override
     public boolean hasError(ClientHttpResponse httpResponse)
             throws IOException {
-        return (httpResponse.getStatusCode().series() == CLIENT_ERROR
-                        || httpResponse.getStatusCode().series() == SERVER_ERROR);
+        return (httpResponse.getStatusCode().is4xxClientError()
+                        || httpResponse.getStatusCode().is5xxServerError());
     }
 
     @Override
